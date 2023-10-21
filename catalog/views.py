e6e1122b0fd73e8714dc75ab4103ca01d1138746
Belish88 <belish888@rambler.ru>
2023-10-21 18:58:15 +0400

    hw20.2 2

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 
+from catalog.models import Category, Product
+
 
 def home(request):
-    return render(request, 'catalog/home.html')
+    context = {
+        'object_list': Category.objects.all(),
+        'title': 'Категории товаров'
+    }
+    return render(request, 'catalog/home.html', context)
+
+
+def product(request):
+    context = {
+        'object_list': Product.objects.filter(),
+        'title': 'Все товары категории '
+    }
+    return render(request, 'catalog/home.html', context)
 
 
 def contacts(request):
